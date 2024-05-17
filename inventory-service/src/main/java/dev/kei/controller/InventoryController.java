f60ee3be91c74640e6b68e0f1b918435bc0c1be1
Kei-K23 <keiksl2301@gmail.com>
2024-05-17 18:46:07 +0630

    Add rate limiting to inventory service

--- inventory-service/src/main/java/dev/kei/controller/InventoryController.java
+++ inventory-service/src/main/java/dev/kei/controller/InventoryController.java
@@ -3,14 +3,19 @@ package dev.kei.controller;
 import dev.kei.dto.InventoryRequestDto;
 import dev.kei.dto.InventoryResponseDto;
 import dev.kei.service.InventoryService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/v1/inventory")
+@Slf4j
 public class InventoryController {
     private final InventoryService inventoryService;
 
@@ -19,38 +24,115 @@ public class InventoryController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public InventoryResponseDto save(@Valid @RequestBody InventoryRequestDto inventoryRequestDto) {
-        return inventoryService.save(inventoryRequestDto);
+    @RateLimiter(name = "inventory-service", fallbackMethod = "saveFallback")
+    public ResponseEntity<InventoryResponseDto> save(@Valid @RequestBody InventoryRequestDto inventoryRequestDto) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(inventoryService.save(inventoryRequestDto));
+        } catch (Exception ex) {
+            if(ex instanceof NoSuchElementException) {
+                throw new NoSuchElementException(ex.getMessage());
+            } else {
+                throw new RuntimeException(ex.getMessage());
+            }
+        }
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<InventoryResponseDto> findAllInventoryItems() {
-        return inventoryService.findAllInventoryItems();
+    @RateLimiter(name = "inventory-service", fallbackMethod = "findAllInventoryItemsFallback")
+    public ResponseEntity<List<InventoryResponseDto>> findAllInventoryItems() {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(inventoryService.findAllInventoryItems());
+        } catch (Exception ex) {
+            if(ex instanceof NoSuchElementException) {
+                throw new NoSuchElementException(ex.getMessage());
+            } else {
+                throw new RuntimeException(ex.getMessage());
+            }
+        }
     }
 
     @GetMapping(params = "productId")
-    @ResponseStatus(HttpStatus.OK)
-    public InventoryResponseDto findInventoryItemByProductId(@RequestParam(name = "productId") String productId) {
-        return inventoryService.findInventoryItemByProductId(productId);
+    @RateLimiter(name = "inventory-service", fallbackMethod = "findInventoryItemByIdFallback")
+    public ResponseEntity<InventoryResponseDto> findInventoryItemByProductId(@RequestParam(name = "productId") String productId) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(inventoryService.findInventoryItemByProductId(productId));
+        } catch (Exception ex) {
+            if(ex instanceof NoSuchElementException) {
+                throw new NoSuchElementException(ex.getMessage());
+            } else {
+                throw new RuntimeException(ex.getMessage());
+            }
+        }
     }
 
     @GetMapping(value = "/check", params = "productIdList")
-    @ResponseStatus(HttpStatus.OK)
-    public List<InventoryResponseDto> checkIsStockEnough(@RequestParam(name = "productIdList") List<String> productIdList) {
-        return inventoryService.checkIsStockEnough(productIdList);
+    @RateLimiter(name = "inventory-service", fallbackMethod = "checkIsStockEnoughFallback")
+    public ResponseEntity<List<InventoryResponseDto>> checkIsStockEnough(@RequestParam(name = "productIdList") List<String> productIdList) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(inventoryService.checkIsStockEnough(productIdList));
+        } catch (Exception ex) {
+            if(ex instanceof NoSuchElementException) {
+                throw new NoSuchElementException(ex.getMessage());
+            } else {
+                throw new RuntimeException(ex.getMessage());
+            }
+        }
     }
 
     @PutMapping(params = "productId")
-    @ResponseStatus(HttpStatus.OK)
-    public InventoryResponseDto update(@RequestParam(name = "productId") String productId,@Valid @RequestBody InventoryRequestDto inventoryRequestDto) {
-        return inventoryService.update(productId, inventoryRequestDto);
+    @RateLimiter(name = "inventory-service", fallbackMethod = "updateFallback")
+    public ResponseEntity<InventoryResponseDto> update(@RequestParam(name = "productId") String productId,@Valid @RequestBody InventoryRequestDto inventoryRequestDto) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(inventoryService.update(productId, inventoryRequestDto));
+        } catch (Exception ex) {
+            if(ex instanceof NoSuchElementException) {
+                throw new NoSuchElementException(ex.getMessage());
+            } else {
+                throw new RuntimeException(ex.getMessage());
+            }
+        }
     }
 
     @DeleteMapping(params = "productId")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RateLimiter(name = "inventory-service")
     public void delete(@RequestParam(name = "productId") String productId) {
         inventoryService.delete(productId);
+    }
+
+    // Fallback methods
+    public ResponseEntity<InventoryResponseDto> saveFallback(InventoryRequestDto inventoryRequestDto, Exception ex) {
+        handleFallback(ex);
+        return null;
+    }
+
+    public ResponseEntity<List<InventoryResponseDto>> findAllInventoryItemsFallback(Exception ex) {
+        handleFallback(ex);
+        return null;
+    }
+
+    public ResponseEntity<List<InventoryResponseDto>> checkIsStockEnoughFallback(List<String> productIdList, Exception ex) {
+        handleFallback(ex);
+        return null;
+    }
+
+    public ResponseEntity<InventoryResponseDto> findInventoryItemByIdFallback(String id, Exception ex) {
+        handleFallback(ex);
+        return null;
+    }
+
+    public ResponseEntity<InventoryResponseDto> updateFallback(String id, InventoryRequestDto inventoryRequestDto, Exception ex) {
+        handleFallback(ex);
+        return null;
+    }
+
+    private void handleFallback(Exception ex) {
+        if (ex instanceof NoSuchElementException) {
+            log.info("NotFoundException in fallback");
+            throw new NoSuchElementException("Inventory product item not found");
+        } else {
+            log.info("Rate limit exceeded");
+            throw new RuntimeException("You have reached your rate limit. Please try again in 30 seconds.");
+        }
     }
 }
